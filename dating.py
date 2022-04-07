d8acbdf7e2aee0f92f69be5dfbbe92e6b24e029f
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-04-07 17:39:15 -0400

    Update dating.py

--- dating.py
+++ dating.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Created by: Huzaifa
-# Created on: March 2022
+# Created on: April 2022
 # This function asks a user if they are rich and handsome
 # and depending on their answeres it tells if they can date
 # the grandchild
@@ -14,18 +14,18 @@ def main():
 
     # input
     user_wealth = input("Are you rich?(yes or no): ")
-    user_looks = input("Are you handsome?(yes or no): ")
+    user_looking = input("Are you handsome?(yes or no): ")
     print("")
 
     # process and output
-    if user_wealth == "yes" or user_looks == "yes":
+    if user_wealth == "yes" or user_looking == "yes":
         print("You can date the grandchild.")
-    elif user_wealth == "no" and user_looks == "no":
+    elif user_wealth == "no" and user_looking == "no":
         print("You can not date the grandchild.")
     else:
-        print("Sorry big man I got nothing ¯\_(ツ)_/¯.")
+        print("Really? (-_-)ゞ all you had to do was say yes or no.")
         print("")
-    print("Thanks for checking.")
+
     print("\nDone.")
 
 
